f1926cc13b35c42562631ba342289a909bccd076
Rohit <roh.boh43@gmail.com>
2024-05-29 22:10:21 +0530

    separate url

--- product_curd/urls.py
+++ product_curd/urls.py
@@ -4,5 +4,7 @@ from product_curd.views import ProductCurd
 
 urlpatterns = [
     path('product/getAll/', ProductCurd.as_view(),name="get all product"),
-    path('product/<product_id>/', ProductCurd.as_view(),name="update and get product"),
+    path('product/<product_id>/update/', ProductCurd.as_view(),name="update product"),
+    path('product/<product_id>/delete/', ProductCurd.as_view(),name="delete product"),
+    path('product/<product_id>/get/', ProductCurd.as_view(),name="get product"),
 ]
